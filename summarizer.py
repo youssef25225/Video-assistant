b47e40f8ebfe8365c56a493050d0d7356b341bd4
Yousef Emam <110260642+youssef25225@users.noreply.github.com>
2026-03-15 09:22:48 +0200

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from transformers import pipeline
 from nltk.tokenize import sent_tokenize
+import os
 import nltk
 
-nltk.download("punkt", quiet=True)
-nltk.download("punkt_tab", quiet=True)
+nltk_data_dir = os.path.join(os.path.expanduser("~"), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.append(nltk_data_dir)
+
+nltk.download("punkt", download_dir=nltk_data_dir, quiet=True)
+nltk.download("punkt_tab", download_dir=nltk_data_dir, quiet=True)
 
 CHUNK_SIZE = 250
 
